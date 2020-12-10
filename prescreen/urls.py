4b237c730744b6632118de2564be38826c46e090
Peter Stenger <peter.a.stenger@gmail.com>
2020-12-10 14:23:52 -0500

    asdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf

--- prescreen/urls.py
+++ prescreen/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('event/<int:pk>/details', views.EventDetailView.as_view(), name='event-detail')
+    path('event/create', views.EventCreate.as_view(), name='event-create'),
+    path('event/<uuid:pk>/', views.EventDetail.as_view(), name='event-detail'),
+    path('event/<uuid:pk>/update', views.EventUpdate.as_view(), name='event-update'),
     # path('event/<int:pk>/responses', views.EventDetailView.as_view(), name='event-detail')
 ]
--- prescreen/views.py
+++ prescreen/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Event, Account, CustomQuestionResponse, Response, CustomQuestion
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def home(request):
     ''' home page '''
@@ -11,5 +12,19 @@ def home(request):
     
     return render(request, "prescreen/home.html", context=context)
 
-class EventDetailView(generic.DetailView):
+class EventDetail(LoginRequiredMixin, generic.DetailView):
     model = Event
+    template_name = "prescreen/event_detail.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_creator'] = self.request.user.id == self.object.creator.id
+        return context
+
+class EventUpdate(generic.UpdateView):
+    model = Event
+    fields = '__all__'
+
+class EventCreate(generic.CreateView):
+    # uses modelname_form.html
+    model = Event
+    fields = ['title', 'start_time']
